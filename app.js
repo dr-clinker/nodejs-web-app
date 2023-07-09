cf3e1b3862fe7a4c62ce1b747eed0ffcd451b2f8
dr-clinker <asa-khb@mail.ru>
2023-07-09 19:45:09 +1000

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
   res.send('Привет, это веб-сайт!');
 });
 
+app.get('/test', (req, res) => {
+  res.send('test');
+});
 // Запуск сервера на порту 3000
 app.listen(3000, () => {
   console.log('Сервер запущен на порту 3000');
